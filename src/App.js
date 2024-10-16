6a3d4ae4677f34210bad8d92e4bf5878ad6d09b4
Kisay Marrugo <marrugokisay@gmmail.com>
2024-10-15 22:24:41 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import LoanForm from './components/LoanForm';
+import LoanList from './components/LoanList';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Contabilidad de Préstamos</h1>
+            <LoanForm />
+            <LoanList />
+        </div>
+    );
+};
+
+export default App;
+cd /ruta/a/tu/proyecto/kisay_marrugo_aa4_ev03
+git --
